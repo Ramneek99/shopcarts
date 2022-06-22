29b25444d4718ff6f97cad26bd9aad577021eaa4
XinpingHuangE <xinpinghuangevang@gmail.com>
2022-06-22 00:49:17 -0400

    add feature to create a shopcart

--- service/routes.py
+++ service/routes.py
@@ -28,12 +28,40 @@ def index():
         "Reminder: return some useful information in json format about the service here",
         status.HTTP_200_OK,
     )
+######################################################################
+# CREATE A NEW SHOPCART
+######################################################################
+@app.route("/shopcarts", methods=["POST"])
+def create_shopcarts():
+    """
+    Creates a Shopcart
+    This endpoint will create a Shopcart based the data in the body that is posted
+    """
+    app.logger.info("Request to create a shopcart")
+    check_content_type("application/json")
+    shopcart = Shopcart()
+    shopcart.deserialize(request.get_json())
+    shopcart.create()
+    message = shopcart.serialize()
+    location_url = url_for("get_shopcarts", shopcart_id=shopcart.id, _external=True)
 
+    app.logger.info("Shopcart with ID [%s] created.", shopcart.id)
+    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
 
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type and content_type == media_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", content_type)
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        "Content-Type must be {}".format(media_type),
+    )
 
 def init_db():
     """ Initializes the SQLAlchemy app """
