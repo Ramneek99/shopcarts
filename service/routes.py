d8421cf2af414611462c943b0b09b5b7204f25f6
XinpingHuangE <xinpinghuangevang@gmail.com>
2022-06-22 01:18:26 -0400

    add feature to list all shopcarts

--- service/routes.py
+++ service/routes.py
@@ -28,7 +28,27 @@ def index():
         "Reminder: return some useful information in json format about the service here",
         status.HTTP_200_OK,
     )
+    
+######################################################################
+# LIST ALL SHOPCARTS
+######################################################################
+@app.route("/shopcarts", methods=["GET"])
+def list_shopcarts():
+    """Returns all of the Shopcarts"""
+    app.logger.info("Request for shopcart list")
+    shopcarts = []
+    category = request.args.get("category")
+    name = request.args.get("name")
+    if category:
+        shopcarts = Shopcart.find_by_category(category)
+    elif name:
+        shopcarts = Shopcart.find_by_name(name)
+    else:
+        shopcarts = Shopcart.all()
 
+    results = [shopcart.serialize() for shopcart in shopcarts]
+    app.logger.info("Returning %d shopcarts", len(results))
+    return jsonify(results), status.HTTP_200_OK
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
