f7a9418609cb4dc3211ffd18615141fc8e7d1970
XinpingHuangE <xinpinghuangevang@gmail.com>
2022-06-23 21:56:21 -0400

    some changes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -48,8 +48,8 @@ class TestYourResourceServer(TestCase):
     def tearDown(self):
         db.session.remove()
 
-    def _create_pets(self, count):
-        """Factory method to create pets in bulk"""
+    def _create_shopcarts(self, count):
+        """Factory method to create shopcarts in bulk"""
         shopcarts = []
         for _ in range(count):
             test_shopcart = ShopcartFactory()
@@ -67,6 +67,34 @@ class TestYourResourceServer(TestCase):
     ######################################################################
 
     def test_index(self):
-        """It should call the home page"""
-        resp = self.app.get("/")
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        """It should call the Home Page"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], "Shopcart Demo REST API Service")
+    def test_create_shopcart(self):
+        """It should Create a new Pet"""
+        test_shopcart = ShopcartFactory()
+        logging.debug("Test Shopcart: %s", test_shopcart.serialize())
+        response = self.client.post(BASE_URL, json=test_shopcart.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = response.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_shopcart = response.get_json()
+        self.assertEqual(new_pet["name"], test_shopcart.name)
+        self.assertEqual(new_pet["category"], test_pet.category)
+        self.assertEqual(new_pet["available"], test_pet.available)
+        self.assertEqual(new_pet["gender"], test_pet.gender.name)
+
+        # Check that the location header was correct
+        response = self.client.get(location)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_pet = response.get_json()
+        self.assertEqual(new_pet["name"], test_pet.name)
+        self.assertEqual(new_pet["category"], test_pet.category)
+        self.assertEqual(new_pet["available"], test_pet.available)
+        self.assertEqual(new_pet["gender"], test_pet.gender.name)
