d0c183d726c06d0bf451b0ef6dacf76a19e1a9c1
XinpingHuangE <xinpinghuangevang@gmail.com>
2022-08-08 13:01:49 -0400

    almost

--- service/utils/error_handlers.py
+++ service/utils/error_handlers.py
@@ -19,10 +19,20 @@ Module: error_handlers
 """
 from flask import jsonify
 from service.models import DataValidationError
-from service import app
+from service import app,api
 from . import status
 
-
+@api.errorhandler(DataValidationError)
+def request_validation_error(error):
+    """ Handles Value Errors from bad data """
+    message = str(error)
+    app.logger.error(message)
+    return {
+        'status_code': status.HTTP_400_BAD_REQUEST,
+        'error': 'Bad Request',
+        'message': message
+    }, status.HTTP_400_BAD_REQUEST
+'''
 ######################################################################
 # Error Handlers
 ######################################################################
@@ -99,7 +109,7 @@ def mediatype_not_supported(error):
         ),
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
     )
-
+'''
 
 @app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
 def internal_server_error(error):
