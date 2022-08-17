ee3d4673c2e911dafca6f884f1ed1b11d580017f
AntonioTan <congmi@outlook.com>
2022-08-16 21:58:55 -0700

    add new test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -19,7 +19,7 @@ from service.models import db, Shopcart, Product
 from service.utils import status  # HTTP Status Codes
 from tests.factories import ShopCartFactory, ProductFactory
 from urllib.parse import quote_plus
-
+from flask import Flask
 logging.disable(logging.CRITICAL)
 
 DATABASE_URI = os.getenv(
@@ -34,6 +34,13 @@ CONTENT_TYPE_JSON = "application/json"
 #  T E S T   C A S E S
 ######################################################################
 
+
+class CustomFlask(Flask):
+    """Custom Flask app for test"""
+    def test_request_context(self, *args, **kwargs):
+        headers = kwargs.setdefault("headers", {})
+        headers.setdefault("Content-Type", CONTENT_TYPE_JSON)
+        return super().test_request_context(*args, **kwargs)
 
 class TestShopcartService(TestCase):
     """Shop Cart Service Tests"""
@@ -235,10 +242,11 @@ class TestShopcartService(TestCase):
     def test_get_shopcart_list(self):
         """It should Get a list of shopcarts"""
         self._create_shopcarts(5)
-        resp = self.client.get(BASE_URL, id=1)
+        resp = self.client.get(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
+        resp = self.client.get(f"{BASE_URL}?id={data[0]['id']}")
 
     def test_get_shopcart_by_id(self):
         """It should Get a shop cart by customer id"""
@@ -463,3 +471,8 @@ class TestShopcartService(TestCase):
         self.assertEqual(len(updated_shopcart["products"]), 1)
         resp = self.client.put(f"{BASE_URL}/{shopcart.id+100}", json=returned_shopcart)
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_check_content_type(self):
+        customApp = CustomFlask(import_name="Test App")
+        with customApp.test_request_context():
+            routes.check_content_type(CONTENT_TYPE_JSON)
